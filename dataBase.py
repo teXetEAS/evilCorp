d5a15b40301b62cc2ae5423c042fb13f175505fc
texet <texet1993texet@yandex.ru>
2020-07-21 19:29:50 +0400

    update ServerDocs, new createNewUser.py

--- dataBase.py
+++ dataBase.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-connection = pymysql.connect(host="localhost",
-                       user="devArhNet",
+connection = pymysql.connect(host="192.168.0.149",
+                       user="workArhNet",
                        password="ebuciyShmel",
                        db="homeEvilCorp",
                        charset='utf8',
                        port=3306)
-with connection.cursor() as cursor:
-	sql = 'select * from usersEvilCorp'
-	cursor.execute(sql)
-	for row in cursor:
-		print(row)
-		connection.close()
+cursor = connection.cursor()   
+sql =  "Insert into users (id, ip, mac, fullName, position, email, password) values (%s, %s, %s, %s, %s, %s, %s)"
+     
+    # Выполнить sql и передать 3 параметра.
+cursor.execute(sql, (1, '192.168.0.109', '90:48:9a:bf:cb:b9', 'Eliseev A.S.', 'CEO', 'eliseevCEO@evilcorp', 'pizdatyipass') )
+     
+connection.commit() 
+connection.close()
